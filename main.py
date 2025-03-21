eefe9d4976a307c8e3e6897aedd059d8d8fa8a82
marcosnevary <marcos.nevary@gmail.com>
2025-03-20 22:22:08 -0300

    style: altera largura mínima da janela

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def main(pagina: ft.Page):
     pagina.title = "articleDB"
     pagina.on_route_change = articledb.controle.controle_de_rota
     pagina.theme_mode = "light"
-    pagina.window.min_width = 750
-    pagina.window.min_height = 700
+    pagina.window.min_width = 800
+    pagina.window.min_height = 600
     pagina.go('1')
 
 
